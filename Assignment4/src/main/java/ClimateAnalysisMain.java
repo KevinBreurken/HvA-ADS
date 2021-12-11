107e9f94640cd6e72fae469c369ef8e5fcd6f4ef
Kevin Breurken <kevinbreurken@gmail.com>
2021-12-11 16:49:55 +0100

    Continued working on the assignment with Irene irl

--- Assignment4/src/main/java/ClimateAnalysisMain.java
+++ Assignment4/src/main/java/ClimateAnalysisMain.java
@@ -13,30 +13,31 @@ public class ClimateAnalysisMain {
         climateTracker.importClimateDataFromVault(ClimateAnalysisMain.class.getResource("/knmi").getPath());
         System.out.println(climateTracker.getStations());
 
-        System.out.println("\n1. Total Number of measurements by station:\n" +
-                climateTracker.numberOfMeasurementsByStation());
+//        System.out.println("\n1. Total Number of measurements by station:\n" +
+//                climateTracker.numberOfMeasurementsByStation());
 //        System.out.println("\n2. First day of measurement by station:\n" +
 //                climateTracker.firstDayOfMeasurementByStation());
-//
 //        System.out.printf("\n3. All-time maximum temperature in de Bilt = %.1f degC\n",
 //                climateTracker.findStationById(260).allTimeMaxTemperature());
-//
+
 //        System.out.println("\n4. Number of valid daily precipitation measurements by station:\n" +
 //                climateTracker.numberOfValidValuesByStation(Measurement::getPrecipitation));
 //        System.out.printf("\n5. Total precipication in de Bilt in 1963 = %.0f mm\n",
 //                climateTracker.findStationById(260).totalPrecipitationBetween(LocalDate.of(1963,1,1), LocalDate.of(1963,12,31)));
-//
+
+        //TODO
 //        System.out.printf("\n6. Annual trend of average temperatures (in degC):\n%s\n",
 //                climateTracker.annualAverageTemperatureTrend());
 //        System.out.printf("\n7. Annual trend of maximum hourly precipitation (in mm):\n%s\n",
 //                climateTracker.annualMaximumTrend(Measurement::getMaxHourlyPrecipitation));
 //        System.out.printf("\n8. Annual trend of maximum wind gust (in m/s):\n%s\n",
 //                climateTracker.annualMaximumTrend(Measurement::getMaxWindGust));
-//
+
 //        System.out.printf("\n9. All-time monthly profile of daily solar hours:\n%s\n",
 //                climateTracker.allTimeAverageDailySolarByMonth());
-//
-//        System.out.printf("\n10. Coldest year = %d\n", climateTracker.coldestYear());
+//      //END OF TOD-o
+
+        System.out.printf("\n10. Coldest year = %d\n", climateTracker.coldestYear());
 
         //mapToCSV("Annual average temperature trend", climateTracker.annualAverageTemperatureTrend());
         //mapToCSV("Annual max hourly precipitation trend", climateTracker.annualMaximumTrend(Measurement::getMaxHourlyPrecipitation));
--- Assignment4/src/main/java/models/ClimateTracker.java
+++ Assignment4/src/main/java/models/ClimateTracker.java
@@ -6,178 +6,26 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class ClimateTracker {
     private final String MEASUREMENTS_FILE_PATTERN = ".*\\.txt";
 
-    private Map<Integer,Station> stations;        // all available weather stations organised by Station Number (STN)
-
-    public Set<Station> getStations() {
-        return new HashSet<>(stations.values());
-    }
-
-    public Station findStationById(int stn) {
-        // TODO find the station with the given stn
-
-
-        return null;
-    }
+    private Map<Integer, Station> stations;        // all available weather stations organised by Station Number (STN)
 
     public ClimateTracker() {
         this.stations = new HashMap<>();
     }
 
     /**
-     * calculates for each station how many Measurement instances have been registered
-     * @return
-     */
-    public Map<Station,Integer> numberOfMeasurementsByStation() {
-        // TODO build a map resolving for each station its number of registered Measurement instances
-        Map<Station, Integer> map = new HashMap<>();
-
-        stations.forEach((k, v) -> map.put(v, v.getMeasurements().size()));
-
-        return map;
-    }
-
-    /**
-     * calculates for each station the date of the first day of its measurements
-     * stations without measurements shall be excluded from the result
-     * @return
-     */
-    public Map<Station,LocalDate> firstDayOfMeasurementByStation() {
-        // TODO build a map resolving for each station the date of its first day of measurements
-
-
-        return null;
-    }
-
-    /**
-     * calculates for each station how many valid values it has available for the measurement quantity
-     * that can be accessed by the mapper.
-     * invalid values are registered as Double.NaN. They originate from empty or corrupt data in the source file
-     * @param mapper    a getter method that accesses the selected quantity from a Measurement instance
-     * @return
-     */
-    public Map<Station,Integer> numberOfValidValuesByStation(Function<Measurement,Double> mapper) {
-        // TODO build a map resolving for each station the number of valid values for the specified quantity.
-
-
-        return null;
-    }
-
-    /**
-     * Calculates for each calendar year in the dataset the average daily temperatures
-     * across all days in the year and all stations in this tracker
-     * (invalid values shall be excluded from the averaging)
-     * @return      a map(Y,T) that provides for each year Y the average temperature T of that year
-     */
-    public Map<Integer,Double> annualAverageTemperatureTrend() {
-        // TODO build a map collecting for each year the average temperature in that year
-
-
-        return null;
-    }
-
-    /**
-     * Calculates for each calendar year in the dataset the maximum of the selected daily quantity
-     * across all days in the year and all stations in this tracker
-     * (invalid values shall be excluded from the maximum aggregation)
-     * (this method can be reused for maximum aggregation of different quantities in the Measurement data
-     * @param mapper a getter function on the Measurement class
-     *              that selects the appropriate value for the maximum aggregation procedure
-     * @return      a map(Y,Q) that provides for each year Y the maximum value Q of the specified quantity
-     */
-    public Map<Integer,Double> annualMaximumTrend(Function<Measurement,Double> mapper) {
-        // TODO build a map collecting for each year the maximum value of the mapped quantity in that year
-
-
-        return null;
-    }
-
-    /**
-     * Calculates for each of the 12 calendar months the average daily hours of sunshine
-     * across all years and all stations
-     * The graph of these numbers can be found in touristic brochures.
-     * (invalid values shall be excluded from the averaging)
-     * @return      a map(M,SQ) that provides for each month M the average daily sunshine hours SQ across all times
-     */
-    public Map<Month,Double> allTimeAverageDailySolarByMonth() {
-        // TODO build a map collecting for each month the average value of daily sunshine hours
-
-
-        return null;
-    }
-
-    /**
-     * calculate the coldest year of all times.
-     * The coldest year is defined as the year with the lowest total sum of daily minimum temperatures below zero
-     * accumulated across all stations
-     * I.e. any daily value of a minimum temperature at a station that is above zero should not be included in its sum for the year
-     * The lowest yearsum of negative minimum temperatures indicates the coldest year.
-     * @return      the coldest year (a number between 1900 and 2099)
-     *              return -1 if no valid minimum temperature measurements are available
-     */
-    public int coldestYear() {
-        // TODO determine the coldest year
-        //  hint: first build a helper map that accumulates the yearsums of negative minimum temperatures
-        //        then find the coldest year in that helper map
-
-
-
-        return -1;
-    }
-
-    /**
-     * imports all station and measurement information
-     * @param folderPath
-     */
-    public void importClimateDataFromVault(String folderPath) {
-        this.stations.clear();
-
-        // load all stations from the text file
-        importItemsFromFile(this.stations,
-                folderPath + "/stations.txt", null,
-                Station::fromLine, Station::getStn);
-
-        // load all measurements from the folder
-        importMeasurementsFromVault(folderPath + "/measurements");
-    }
-
-    /**
-     * traverses the purchases vault recursively and processes every data file that it finds
-     * @param filePath
-     */
-    public void importMeasurementsFromVault(String filePath) {
-
-        File file = new File(filePath);
-
-        if (file.isDirectory()) {
-            // the file is a folder (a.k.a. directory)
-            //  retrieve a list of all files and sub folders in this directory
-            File[] filesInDirectory = Objects.requireNonNullElse(file.listFiles(), new File[0]);
-
-            // merge all purchases of all files and sub folders from the filesInDirectory list, recursively.
-            for (File f : filesInDirectory) {
-                this.importMeasurementsFromVault(f.getAbsolutePath());
-            }
-        } else if (file.getName().matches(MEASUREMENTS_FILE_PATTERN)) {
-            // the file is a regular file that matches the target pattern for raw purchase files
-            // merge the content of this file into this.purchases
-            this.importMeasurementsFromFile(file.getAbsolutePath());
-        }
-    }
-
-    /**
      * imports a collection of items from a text file which provides one line for each item
-     * @param items         the list to which imported items shall be added
-     * @param filePath      the file path of the source text file
-     * @param converter     a function that can convert a text line into a new item instance
-     * @param <E>           the (generic) type of each item
+     *
+     * @param items     the list to which imported items shall be added
+     * @param filePath  the file path of the source text file
+     * @param converter a function that can convert a text line into a new item instance
+     * @param <E>       the (generic) type of each item
      */
-    private static <K,E> void importItemsFromFile(Map<K,E> items, String filePath, String headerPrefix,
-                                                 Function<String,E> converter, Function<E,K> mapper) {
+    private static <K, E> void importItemsFromFile(Map<K, E> items, String filePath, String headerPrefix,
+                                                   Function<String, E> converter, Function<E, K> mapper) {
         int originalNumItems = items.size();
 
         Scanner scanner = createFileScanner(filePath);
@@ -212,7 +60,7 @@ public class ClimateTracker {
             }
         }
 
-        if (items.size() < originalNumItems+newCount) {
+        if (items.size() < originalNumItems + newCount) {
             throw new InputMismatchException(String.format("Duplicate items found in file %s", filePath));
         }
 
@@ -220,8 +68,201 @@ public class ClimateTracker {
     }
 
     /**
+     * helper method to create a scanner on a file an handle the exception
+     *
+     * @param filePath
+     * @return
+     */
+    private static Scanner createFileScanner(String filePath) {
+        try {
+            return new Scanner(new File(filePath));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("FileNotFound exception on path: " + filePath);
+        }
+    }
+
+    public Set<Station> getStations() {
+        return new HashSet<>(stations.values());
+    }
+
+    public Station findStationById(int stn) {
+        return stations.get(stn);
+    }
+
+    /**
+     * calculates for each station how many Measurement instances have been registered
+     *
+     * @return
+     */
+    public Map<Station, Integer> numberOfMeasurementsByStation() {
+        Map<Station, Integer> map = new HashMap<>();
+
+        stations.forEach((k, v) -> map.put(v, v.getMeasurements().size()));
+
+        return map;
+    }
+
+    /**
+     * calculates for each station the date of the first day of its measurements
+     * stations without measurements shall be excluded from the result
+     *
+     * @return
+     */
+    public Map<Station, LocalDate> firstDayOfMeasurementByStation() {
+        // TODO build a map resolving for each station the date of its first day of measurements
+        Map<Station, LocalDate> map = new HashMap<>();
+
+        getStations().forEach(station -> {
+            Optional<LocalDate> firstDayOfMeasurements = station.firstDayOfMeasurement();
+            firstDayOfMeasurements.ifPresent(localDate -> map.put(station, localDate));
+        });
+
+        return map;
+    }
+
+    /**
+     * calculates for each station how many valid values it has available for the measurement quantity
+     * that can be accessed by the mapper.
+     * invalid values are registered as Double.NaN. They originate from empty or corrupt data in the source file
+     *
+     * @param mapper a getter method that accesses the selected quantity from a Measurement instance
+     * @return
+     */
+    public Map<Station, Integer> numberOfValidValuesByStation(Function<Measurement, Double> mapper) {
+        Map<Station, Integer> map = new HashMap<>();
+
+        stations.values().forEach(station -> map.put(
+                station,
+                (int) station.getMeasurements().stream().filter(measurement -> !mapper.apply(measurement).isNaN()).count()
+        ));
+
+        return map;
+    }
+
+    /**
+     * Calculates for each calendar year in the dataset the average daily temperatures
+     * across all days in the year and all stations in this tracker
+     * (invalid values shall be excluded from the averaging)
+     *
+     * @return a map(Y,T) that provides for each year Y the average temperature T of that year
+     */
+    public Map<Integer, Double> annualAverageTemperatureTrend() {
+        // TODO build a map collecting for each year the average temperature in that year
+        Map<Integer, Double> map = new HashMap<>();
+
+
+        return map;
+    }
+
+    /**
+     * Calculates for each calendar year in the dataset the maximum of the selected daily quantity
+     * across all days in the year and all stations in this tracker
+     * (invalid values shall be excluded from the maximum aggregation)
+     * (this method can be reused for maximum aggregation of different quantities in the Measurement data
+     *
+     * @param mapper a getter function on the Measurement class
+     *               that selects the appropriate value for the maximum aggregation procedure
+     * @return a map(Y,Q) that provides for each year Y the maximum value Q of the specified quantity
+     */
+    public Map<Integer, Double> annualMaximumTrend(Function<Measurement, Double> mapper) {
+        // TODO build a map collecting for each year the maximum value of the mapped quantity in that year
+
+
+        return null;
+    }
+
+    /**
+     * Calculates for each of the 12 calendar months the average daily hours of sunshine
+     * across all years and all stations
+     * The graph of these numbers can be found in touristic brochures.
+     * (invalid values shall be excluded from the averaging)
+     *
+     * @return a map(M,SQ) that provides for each month M the average daily sunshine hours SQ across all times
+     */
+    public Map<Month, Double> allTimeAverageDailySolarByMonth() {
+        // TODO build a map collecting for each month the average value of daily sunshine hours
+
+
+        return null;
+    }
+
+    /**
+     * calculate the coldest year of all times.
+     * The coldest year is defined as the year with the lowest total sum of daily minimum temperatures below zero
+     * accumulated across all stations
+     * I.e. any daily value of a minimum temperature at a station that is above zero should not be included in its sum for the year
+     * The lowest yearsum of negative minimum temperatures indicates the coldest year.
+     *
+     * @return the coldest year (a number between 1900 and 2099)
+     * return -1 if no valid minimum temperature measurements are available
+     */
+    public int coldestYear() {
+        // TODO determine the coldest year
+        //  hint: first build a helper map that accumulates the yearsums of negative minimum temperatures
+        //        then find the coldest year in that helper map
+
+        Map<Integer, Double> map = new HashMap<>();
+
+        stations.values().forEach(s -> {
+
+        });
+
+        return 0;
+//        Map<Station, Long> map = new HashMap<>();
+//
+//        stations.values().forEach(s ->
+//                map.put(s, s.getMeasurements().stream().filter(m -> m.getMinTemperature() < 0).count())
+//        );
+//
+//        return map.values().stream().
+    }
+
+    /**
+     * imports all station and measurement information
+     *
+     * @param folderPath
+     */
+    public void importClimateDataFromVault(String folderPath) {
+        this.stations.clear();
+
+        // load all stations from the text file
+        importItemsFromFile(this.stations,
+                folderPath + "/stations.txt", null,
+                Station::fromLine, Station::getStn);
+
+        // load all measurements from the folder
+        importMeasurementsFromVault(folderPath + "/measurements");
+    }
+
+    /**
+     * traverses the purchases vault recursively and processes every data file that it finds
+     *
+     * @param filePath
+     */
+    public void importMeasurementsFromVault(String filePath) {
+
+        File file = new File(filePath);
+
+        if (file.isDirectory()) {
+            // the file is a folder (a.k.a. directory)
+            //  retrieve a list of all files and sub folders in this directory
+            File[] filesInDirectory = Objects.requireNonNullElse(file.listFiles(), new File[0]);
+
+            // merge all purchases of all files and sub folders from the filesInDirectory list, recursively.
+            for (File f : filesInDirectory) {
+                this.importMeasurementsFromVault(f.getAbsolutePath());
+            }
+        } else if (file.getName().matches(MEASUREMENTS_FILE_PATTERN)) {
+            // the file is a regular file that matches the target pattern for raw purchase files
+            // merge the content of this file into this.purchases
+            this.importMeasurementsFromFile(file.getAbsolutePath());
+        }
+    }
+
+    /**
      * imports another batch of raw purchase data from the filePath text file
      * and merges the purchase amounts with the earlier imported and accumulated collection in this.purchases
+     *
      * @param filePath
      */
     private void importMeasurementsFromFile(String filePath) {
@@ -242,19 +283,6 @@ public class ClimateTracker {
             if (numAdded != newMeasurementsByDate.size()) {
                 throw new InputMismatchException(String.format("Some items in file %s could not be added", filePath));
             }
-        }
-    }
-
-    /**
-     * helper method to create a scanner on a file an handle the exception
-     * @param filePath
-     * @return
-     */
-    private static Scanner createFileScanner(String filePath) {
-        try {
-            return new Scanner(new File(filePath));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("FileNotFound exception on path: " + filePath);
         }
     }
 }
